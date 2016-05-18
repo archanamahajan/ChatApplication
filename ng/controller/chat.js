ff4f75bc5809824042cc040aa55845313e8873f0
Archana Mahajan <archanamahajan10@gmail.com>
2016-05-18 20:34:10 +0530

    Update chat.js

--- ng/controller/chat.js
+++ ng/controller/chat.js
@@ -1,12 +1,7 @@
 angular.module('chatapp')
     .controller('chatCtrl', function($scope, $state, $timeout, $filter){
-        console.log('in chatCtrl');
-        //var socket = io.connect('http://localhost:3000');
-        //var socket = io('http://localhost:3000');
-        //var winHeight = $( window ).height();
         var msgJSON;
         $scope.uname = $state.params.username;
-        //$('#chatWindow').height(winHeight - 80);
         var socket = io();
         $scope.msgArray = [];
         $scope.connectedUsers = [];
@@ -24,7 +19,6 @@ angular.module('chatapp')
         }
         
         $scope.doEndChat = function () {
-            console.log('disconnect');
             $scope.msgArray = [];
             socket.emit('disconnect');
             $state.go('login');
@@ -33,19 +27,14 @@ angular.module('chatapp')
         socket.on('message received', function (msg) {
             $timeout(function() {
                 $scope.msgArray = [];
-                console.log('msg : ', msg);
                 $scope.msgArray = msg;
-                console.log('$scope.msgArray : ', $scope.msgArray);
             }, 0);
         });
         
         socket.on('userConnected', function (data) {
             $timeout(function() {
                 $scope.connectedUsers.push($scope.uname);
-                console.log('in index.html on 123 : ',data);
-                //$scope.statusMsg = $scope.uname + ' is connected to the chat';
                 $scope.statusMsg = 'You can now chat';
-                console.log('$scope.statusMsg : ', $scope.statusMsg); 
                 
                 socket.emit('show status', $scope.uname);
             }, 0);
@@ -53,8 +42,6 @@ angular.module('chatapp')
         
         socket.on('connected users list', function(users) {
             $timeout(function() {
-                console.log('connected users list : ', users);
-                //$scope.connectedUsers = [];
                 $scope.connectedUsers = users;
             }, 0);
         });
